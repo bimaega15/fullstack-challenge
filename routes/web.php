43dc48159826af110f075b51e598031c9f3ea08f
bimaega15 <49235873+bimaega15@users.noreply.github.com>
2024-05-16 08:54:01 +0700

    first setup

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/test-email', function () {
+    \Illuminate\Support\Facades\Mail::raw('This is a test email', function ($message) {
+        $message->to('bimaega15@gmail.com')
+            ->subject('Test Email');
+    });
+
+    return 'Email sent!';
+});
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
